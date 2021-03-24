7f294293f225cee9ffe63f5330e9a9bb4d78a90e
Katherine Mattatall <katherinemattatall@hotmail.com>
2021-03-24 04:03:47 -0400

    added health system

---
+++ zombie game/Assets/Scripts/Health.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public int lives;
+    public int numHearts;
+    public Image[] hearts;
+
+    void Update() {
+        if (lives < 1) {
+            print("player is dead");
+            Destroy(gameObject);
+        }
+        if (lives > numHearts){
+            lives = numHearts;
+        }
+        for(int i = 0; i < hearts.Length; i++){
+            if (i < numHearts) {
+                hearts[i].enabled = true;
+            }
+            else {
+                hearts[i].enabled = false;
+            }
+            if (i >= lives) {
+                hearts[i].enabled = false;
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collider) {
+        if (collider.gameObject.CompareTag("Enemy")){
+            PlayerTakesDamage(1);
+        }
+    }
+
+    public void PlayerTakesDamage(int life) {
+        lives = lives - life;
+    }
+}
