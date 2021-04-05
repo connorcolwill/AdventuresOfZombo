695148a5cc9d4888efd50a583523e46ffceafe3f
KatherineMattatall <katherinemattatall@hotmail.com>
2021-04-05 15:50:01 -0400

    added footsteps and death sound effects

---
+++ zombie game/Assets/Footsteps.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour
+{
+    public AudioClip clip;
+    public AudioSource audsource;
+
+    private void Awake()
+    {
+        audsource = GetComponent<AudioSource>();
+    }
+
+    private void FootstepSound()
+    {
+        audsource.PlayOneShot(clip);
+    }
+}
---
+++ zombie game/Assets/PlayerDeath.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour
+{
+    public AudioClip clip;
+    public AudioSource audsource;
+
+    private void Awake()
+    {
+        audsource = GetComponent<AudioSource>();
+    }
+
+    private void DeathSound()
+    {
+        audsource.PlayOneShot(clip);
+    }
+}
