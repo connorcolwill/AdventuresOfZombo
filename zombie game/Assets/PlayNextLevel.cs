1912a37934992270154d0d6f82ad84aa66f9642e
KatherineMattatall <katherinemattatall@hotmail.com>
2021-04-08 20:06:18 -0400

    added level complete screens

---
+++ zombie game/Assets/PlayNextLevel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayNextLevel : MonoBehaviour
+{
+    public string nextLevel;
+
+    public void LevelComplete() 
+    {
+    SceneManager.LoadScene(nextLevel);
+    }
+}
--- zombie game/Assets/Scripts/Gravestone.cs
+++ zombie game/Assets/Scripts/Gravestone.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class Gravestone : MonoBehaviour
 {
-    public string nextLevel;
-    void OnCollisionEnter2D(Collision2D collider) {
-        SceneManager.LoadScene(nextLevel);
+    public GameObject levelCompleteScreen;
+
+    public void OnCollisionEnter2D(Collision2D collider)
+    {
+        levelCompleteScreen.SetActive(true);
     }
 }
